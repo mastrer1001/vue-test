48baa9cf424fe7d6877c9ff1798868a7bad3a8dc
Johannes Boog <j.boog@gmx.de>
2021-05-02 13:28:56 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Spagetti" src="./assets/carbonara.jpg">
+    <HelloWorld message="Spagetti Carbonara" intro="Carbonara ist ein Nudelgericht aus Pasta mit Guanciale, Ei, Pfeffer und Käse aus der italienischen Region Latium. Es gehört zu den Klassikern der italienischen Küche."
+                button="btn1"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <p>
+      {{intro}}
+    </p>
+    <div v-if = "button==='btn1'">
+    <h3>Zutaten</h3>
+    <ul>
+      <li v-for= "item in zutaten" :key="item.zutat">
+        {{item.zutat}}
+      </li>
+    </ul>
+    </div>
+    <div v-else-if="button==='btn2'">
+    <h3>Vorgehen</h3>
+    <ul>
+      <li v-for= "step in vorgehen" :key="step.schritt">
+        {{step.schritt}}
+      </li>
+    </ul>
+    </div>
+    <div class="btn-group">
+      <button v-on:click = "button='btn1'">Zutaten</button>
+      <button v-on:click = "button='btn2'">Vorgehen</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    message: String,
+    button: String,
+    intro: String,
+  },
+  data(){
+    return{
+      zutaten:[
+        {zutat: '200g Spagetti'},
+        {zutat: '50g Parmesan, gerieben'},
+        {zutat: '150g Pancetta oder Schinken'},
+        {zutat: '2 Eier'},
+        {zutat: 'Salz'},
+        {zutat: 'Pfeffer'},
+      ],
+      vorgehen:[
+        {schritt: 'Spagetti in kochendem Salzwasser kochen'},
+        {schritt: 'In der Zwischenzeit Pancetta anbraten bis das Fett ausgelaufen ist'},
+        {schritt: 'Eier mit dem Parmesan und reichlich Pfeffer verquirlen'},
+        {schritt: 'Pancetta aus der Pfanne entfernen und Hitze auf niedrig stellen'},
+        {schritt: 'Die Spagetti sobald sie fertig sind abgießen und mit dem Fett in der Pfanne vermengen'},
+        {schritt: 'Pancetta und Eimischung zügig in die Pfanne geben und verrühren, dabei darauf achten, dass die Eier nicht zu viel kochen. Die Pfanne sollte nicht mehr auf der Herdplatte sein müssen, das Kochen geschieht durch die Hitze der Nudeln.'},
+        {schritt: 'So schnell wie möglich servieren'},
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  text-align: center;
+}
+li {
+  display: block;
+  margin-top: 10px;
+}
+a {
+  color: #42b983;
+}
+.btn-group{
+  width:30%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.btn-group button{
+  width:50%;
+  min-width: fit-content;
+  text-align: center;
+  background-color: #42b983; /* Green background */
+  border: 1px solid green; /* Green border */
+  color: white; /* White text */
+  padding: 10px 24px; /* Some padding */
+  cursor: pointer; /* Pointer/hand icon */
+  float: left; /* Float the buttons side by side */
+}
+
+.btn-group button:not(:last-child) {
+  border-right: none; /* Prevent double borders */
+
+}
+.btn-group button:hover {
+  background-color: #34495e;
+}
+</style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
